ae726fa74d8d00ab8a51dda1a817048f7e55430a
KubaOstrowski <ostrowskikuba@vp.pl>
2017-05-09 13:55:38 +0200

    Update index.php

--- MySQL/index.php
+++ MySQL/index.php
@@ -4,21 +4,37 @@
 	<meta charset="utf-8"/>
 </head>
 <body>
-<?
+<?php
 $connect = mysql_connect("localhost","root","");
 if($connect)
 {
 	$baza = mysql_select_db("korporacja");
 	if($baza)
 	{
-		
+		$wynik = mysql_query("SELECT * FROM pracownicy");
+		if($wynik)
+		{
+			$ile = mysql_num_rows($wynik);
+			if($ile>0)
+			{
+				for($i=1;$i<$ile;$i++)
+				{
+					$wiersz = mysql_fetch_assoc($wynik);
+					$imie = $wiersz["imie"];
+					$nazwisko = $wiersz["nazwisko"];
+					print $imie." ".$nazwisko."<br />";
+				}
+			}
+			else echo "Zapytanie nie zwróciło rekordów";
+		}
+		else echo "BŁĄD zapytania";	
 	}
-	else echo "Brak połączenia z wybraną bazaą":
-}
+	else echo "Brak połączenia z wybraną bazą";
 mysql_close($connect);
+}
 else
 {
-	echo"Brak połączenia";
+	echo "Brak połączenia";
 }
 ?>
 </body>
