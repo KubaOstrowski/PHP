9271da52e7f1e231cabe6162c801d78e7f26edac
KubaOstrowski <ostrowskikuba@vp.pl>
2017-05-15 08:25:59 +0200

    v1

--- MySQL/index.php
+++ MySQL/index.php
@@ -1,24 +1,20 @@
 <!DOCTYPE html>
 <html lang="pl">
 <head>
-	<meta charset="utf-8"/>
+	<meta charset="utf-8" />
 </head>
 <body>
 <?php
 $connect = mysql_connect("localhost","root","");
-if($connect)
-{
+if($connect){
 	$baza = mysql_select_db("korporacja");
-	if($baza)
-	{
+	if($baza){
+		
 		$wynik = mysql_query("SELECT * FROM pracownicy");
-		if($wynik)
-		{
+		if($wynik){
 			$ile = mysql_num_rows($wynik);
-			if($ile>0)
-			{
-				for($i=1;$i<$ile;$i++)
-				{
+			if($ile>0){
+				for($i=1;$i<=$ile;$i++){
 					$wiersz = mysql_fetch_assoc($wynik);
 					$imie = $wiersz["imie"];
 					$nazwisko = $wiersz["nazwisko"];
@@ -27,14 +23,26 @@ if($connect)
 			}
 			else echo "Zapytanie nie zwróciło rekordów";
 		}
-		else echo "BŁĄD zapytania";	
+		else echo "BŁĄD zapytania";
+		
+		$wynik = mysql_query("SELECT * FROM pracownicy");
+		if($wynik){
+				$test=0;
+				while($wiersz = mysql_fetch_assoc($wynik)){
+					$imie = $wiersz["imie"];
+					$nazwisko = $wiersz["nazwisko"];
+					print $imie." ".$nazwisko."<br />";
+					$test = 1;
+				}
+				if($test==0) echo "Zapytanie nie zwróciło rekordów";
+			}
+		else echo "BŁĄD zapytania";
 	}
 	else echo "Brak połączenia z wybraną bazą";
 mysql_close($connect);
 }
-else
-{
-	echo "Brak połączenia";
+else{
+	echo "Brak połączenia z zadanym hostm, spróbuj jeszcze raz";
 }
 ?>
 </body>
