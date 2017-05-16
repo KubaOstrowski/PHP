1e7be2ad350e99cfc1426998da0e65af2f01846f
KubaOstrowski <ostrowskikuba@vp.pl>
2017-05-16 13:11:26 +0200

    v1

---
+++ ProjektowanieObiektowe/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>OOP w PHP</title>
+<meta charset="utf-8" />
+</head>
+<body>
+<?php
+/*
+class Pojazd
+{
+public $v;
+public $v_max;
+
+public function przyspiesz($value)
+{
+$this->v = $this->v + $value; //$this->v += $value
+if($this->v > $this->v_max) $this->v = $this->v_max;
+}
+public function zwolnij($value)
+{
+$this->v -= $value;
+if($this->v < 0)$this->v = 0;
+}
+public function show_v()
+{
+return $this->v;
+}
+}
+class Skrecajacy Extends Pojazd
+{
+public $kierunek;
+public function zwrot($value)
+{
+$this->kierunek += $value;
+}
+}
+*/
+function ladujKlasy($nazwaKlasy)
+{
+require "$nazwaKlasy.php";
+}
+spl_autoload_register('ladujKlasy');
+
+$szynobus = new Pojazd(0, 30);
+$bus = new Skrecajacy(0, 30);
+?>
+</body>
+</html>
