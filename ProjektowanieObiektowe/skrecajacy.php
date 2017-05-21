90aa5c9723febafa80124af2a09a689ac28e6afc
KubaOstrowski <ostrowskikuba@vp.pl>
2017-05-21 16:32:50 +0200

    Update skrecajacy.php

--- ProjektowanieObiektowe/skrecajacy.php
+++ ProjektowanieObiektowe/skrecajacy.php
@@ -1,3 +1,22 @@
 <?php
-
+class Skrecajacy Extends Pojazd {
+	function __construct($v_pocz, $v_mx, $skr){
+		$this->v = $v_pocz;
+		$this->v_max = $v_mx;
+		$this->kierunek += $skr;
+	}
+	
+	public $kierunek;
+	
+	public function zwrot($value){
+		$this->kierunek += $value;
+	}
+	public function zwolnij($value){
+		$this->v = 20;
+		parent::zwolnij(6);
+	}
+	public function show_zwrot(){
+		return $this->kierunek;
+	}
+}
 ?>
